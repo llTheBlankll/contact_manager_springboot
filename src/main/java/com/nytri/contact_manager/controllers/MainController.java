2538aadc48b08becf198140b95c2b89a7b9da73b
Nytria <llTheBlankll@gmail.com>
2022-11-27 21:34:03 +0800

    Controller Update
    Added the ability to search people by birthdate, first name, last name.

--- src/main/java/com/nytri/contact_manager/controllers/MainController.java
+++ src/main/java/com/nytri/contact_manager/controllers/MainController.java
@@ -5,11 +5,9 @@ import com.nytri.contact_manager.entities.Person;
 import com.nytri.contact_manager.repositories.ContactRepository;
 import com.nytri.contact_manager.repositories.PersonRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -34,5 +32,25 @@ public class MainController {
     @RequestMapping(method = RequestMethod.GET, path = "/all_contacts")
     public List<Contact> all_contacts() {
         return this.contactRepository.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/search_people/by_firstName/{first_name}")
+    public Person search_person_by_firstname(@RequestParam("first_name") String first_name) {
+        return this.personRepository.findByFirstNameContaining(first_name);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/search_people/by_lastName/{last_name}")
+    public Person search_person_by_lastname(@RequestParam("last_name") String last_name) {
+        return this.personRepository.findByLastNameContaining(last_name);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/search_people/by_birthday/{date}")
+    public Person search_person_by_birthdate(@RequestParam("date") LocalDate date) {
+        return this.personRepository.findByBirthdate(date);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/search_people/by_birthday_between/{fromDate}/{toDate}")
+    public Person find_people_by_birthdate(@RequestParam("fromDate") LocalDate fromDate, @RequestParam("toDate") LocalDate toDate) {
+        return this.personRepository.findByBirthdateBetween(fromDate, toDate);
     }
 }
