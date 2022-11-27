935b2d4bee92552ce9afff6f4fb1c26eb3e402d6
Nytria <llTheBlankll@gmail.com>
2022-11-27 21:33:27 +0800

    Repository Update
    Added the ability to search person by first name, last name, birthdate.

--- src/main/java/com/nytri/contact_manager/repositories/PersonRepository.java
+++ src/main/java/com/nytri/contact_manager/repositories/PersonRepository.java
@@ -3,6 +3,12 @@ package com.nytri.contact_manager.repositories;
 import com.nytri.contact_manager.entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PersonRepository extends JpaRepository<Person, Integer> {
+import java.time.LocalDate;
+import java.util.Date;
 
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+    Person findByFirstNameContaining(String fname);
+    Person findByLastNameContaining(String lname);
+    Person findByBirthdate(LocalDate birthdate);
+    Person findByBirthdateBetween(LocalDate birthdate, LocalDate birthdate2);
 }
