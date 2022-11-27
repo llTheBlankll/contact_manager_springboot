89e7b47147a453bee15ce2215ed228ca240a914e
Nytria <llTheBlankll@gmail.com>
2022-11-27 18:34:55 +0800

    Initial Commit

---
+++ src/test/java/com/nytri/contact_manager/ContactManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nytri.contact_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
