5b93e0de53874ab429d461b3f013fd9663ac30fb
Nytria <llTheBlankll@gmail.com>
2022-11-30 10:36:11 +0800

    Find by Email that contains and ignore case Find By Phone that contains the phone.

--- src/main/java/com/nytri/contact_manager/repositories/ContactRepository.java
+++ src/main/java/com/nytri/contact_manager/repositories/ContactRepository.java
@@ -3,6 +3,10 @@ package com.nytri.contact_manager.repositories;
 import com.nytri.contact_manager.entities.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ContactRepository extends JpaRepository<Contact, Integer> {
 
+    List<Contact> findAllByEmailContainsIgnoreCase(String email);
+    List<Contact> findByPhoneContains(String phone);
 }
